afb831a3c64cf95634506741ad0d4289862423fa
veer-27 <86522744+veer-27@users.noreply.github.com>
2021-12-09 18:35:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song_1="";
+song_2="";
+
+function preload()
+{
+    song_1 = loadSound("AI MUSIC_music.mp3");
+    song_2 = loadSound("AI MUSIC_music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(550,550);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+}
